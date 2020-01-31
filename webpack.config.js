b5e2f2aa2d5cd258818cbd08e24c14746e649480
lekegitrepo <lekedevrepo@gmail.com>
2020-01-31 13:22:11 -0600

    install clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   //entry: './src/index.js',
@@ -8,6 +9,7 @@ module.exports = {
     print: './src/print.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Webpack Tutorial guide!',
     }),
