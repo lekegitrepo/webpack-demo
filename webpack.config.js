e05afcf92fe4c33472d2990de32fc2bf6370655c
lekegitrepo <lekedevrepo@gmail.com>
2020-01-31 13:19:03 -0600

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   //entry: './src/index.js',
@@ -6,6 +7,11 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Tutorial guide!',
+    }),
+  ],
   output: {
     //filename: 'bundle.js',
     filename: '[name].bundle.js',
